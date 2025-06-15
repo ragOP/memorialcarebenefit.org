8b23572553a52d8222e4703a939aba3129547d4b
ragOP <najmiraghib@gmail.com>
2025-06-15 19:47:08 +0530

    first commit

---
+++ congrats.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Congratulations</title>
+  <link rel="stylesheet" href="stylepage.css">
+<script src="https://b-js.ringba.com/CAfdb7eb5bdaa24fb8a155f293ece397ae" async></script>
+
+
+</head>
+<body>
+  <div class="checking" id="congratulations">
+    <div class="congrats">Congratulations, You Qualify 🎉 </div>
+    <div class="top-description-5">
+      Make A <b>Quick Call</b> To Claim Your $25,000 Life Insurance Benefit Now!  
+    </div>
+    <div class="spots-count">Spots remaining: 4</div>
+    <!-- <div class="call-btn" onclick="handleCall()">
+      <a href="tel:+13214858035">
+        <div class="call-btn">CALL (321) 485-8035</div>
+      </a>
+    </div> -->
+
+    <div class="call-btn" onclick="handleCall()">
+  <a href="tel:+13214858035">CALL (321) 485-8035</a>
+</div>
+
+    <div class="sub-description">
+      Due to high call volume, your official agent is waiting for only <b>3 minutes</b>, then your spot will not be reserved.
+    </div>
+    <div class="timer">
+      <div class="timer-cell" id="minutes">3</div>
+      <div class="timer-cell">:</div>
+      <div class="timer-cell" id="seconds">00</div>
+    </div>
+  </div>
+</body>
+</html>
